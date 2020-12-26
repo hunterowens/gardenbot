c4328c2e8f40e83d8d7955ef2a642200a58757b2
hunterowens <owens.hunter@gmail.com>
2020-12-26 12:05:37 -0800

    uppercase

--- weekly_email.py
+++ weekly_email.py
@@ -5,7 +5,7 @@ import pandas as pd
 import datetime
 import sys
 
-TOKEN = os.environ.get('postmark_api_key')
+TOKEN = os.environ.get('POSTMARK_API_KEY')
 print(TOKEN)
 BASE_URL = 'https://gardenbot-uxehlftuua-uw.a.run.app'
 TODAY = datetime.date.today()
